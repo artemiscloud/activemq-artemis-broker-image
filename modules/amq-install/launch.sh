7ba86f623365d8c84b4cbadce79f0e1a99fcaae0
andytaylor <andy.tayls67@gmail.com>
2018-03-04 12:19:01 +0000

    added clustering and replication feature
    Signed-off-by: andytaylor <andy.tayls67@gmail.com>

--- modules/amq-install/launch.sh
+++ modules/amq-install/launch.sh
@@ -13,7 +13,19 @@ function configure() {
     local instanceId=$2
     export CONTAINER_ID=$HOSTNAME
     if [ ! -d "$INSTANCE" ]; then
-        $AMQ_HOME/bin/artemis create broker --user $AMQ_USER --password $AMQ_PASSWORD --role $AMQ_ROLE --name $AMQ_NAME --allow-anonymous --http-host $BROKER_IP --host $BROKER_IP --java-options "$JAVA_OPTS"
+        AMQ_ARGS="--user $AMQ_USER --password $AMQ_PASSWORD --role $AMQ_ROLE --name $AMQ_NAME --allow-anonymous --http-host $BROKER_IP --host $BROKER_IP "
+        if [ "$AMQ_CLUSTERED" = "true" ]; then
+            echo "Broker will be clustered"
+            AMQ_ARGS="$AMQ_ARGS --clustered --cluster-user=$AMQ_CLUSTER_USER --cluster-password=$AMQ_CLUSTER_PASSWORD"
+            if [ "$AMQ_REPLICATED" = "true" ]; then
+                AMQ_ARGS="$AMQ_ARGS --replicated"
+            fi
+            if [ "$AMQ_SLAVE" = "true" ]; then
+                AMQ_ARGS="$AMQ_ARGS --slave"
+            fi
+        fi
+        echo "Creating Broker with args $AMQ_ARGS"
+        $AMQ_HOME/bin/artemis create broker $AMQ_ARGS
     fi
 
 }

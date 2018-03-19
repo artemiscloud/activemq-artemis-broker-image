b37d5d4a7572f10eaf70da9b2a7acbdb51b6e8ec
Vanessa Busch <vbusch@redhat.com>
2018-03-19 18:12:36 -0400

    Removed the default user & password.  The user will be prompted for one, if not provided.
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- modules/amq-install/added/launch.sh
+++ modules/amq-install/added/launch.sh
@@ -11,8 +11,12 @@ export BROKER_IP=`hostname -I | cut -f 1 -d ' '`
 function configure() {
 
     export CONTAINER_ID=$HOSTNAME
+    
     if [ ! -d "BROKER" -o "$AMQ_RESET_CONFIG" = "true" ]; then
-        AMQ_ARGS="--user $AMQ_USER --password $AMQ_PASSWORD --role $AMQ_ROLE --name $AMQ_NAME --allow-anonymous --http-host $BROKER_IP --host $BROKER_IP "
+        AMQ_ARGS="--role $AMQ_ROLE --name $AMQ_NAME --allow-anonymous --http-host $BROKER_IP --host $BROKER_IP "
+    	if [ -n "${AMQ_USER}" -a -n "${AMQ_PASSWORD}" ] ; then
+			AMQ_ARGS="--user $AMQ_USER --password $AMQ_PASSWORD $AMQ_ARGS "
+		fi
         if [ "$AMQ_CLUSTERED" = "true" ]; then
             echo "Broker will be clustered"
             AMQ_ARGS="$AMQ_ARGS --clustered --cluster-user=$AMQ_CLUSTER_USER --cluster-password=$AMQ_CLUSTER_PASSWORD"
@@ -29,8 +33,11 @@ function configure() {
         if [ "$AMQ_EXTRA_ARGS" ]; then
             AMQ_ARGS="$AMQ_ARGS $AMQ_EXTRA_ARGS"
         fi
-        echo "Creating Broker with args $AMQ_ARGS"
-        $AMQ_HOME/bin/artemis create broker $AMQ_ARGS
+
+        PRINT_ARGS="${AMQ_ARGS/$AMQ_PASSWORD/XXXXX}"
+        PRINT_ARGS="${PRINT_ARGS/$AMQ_USER/XXXXX}"
+        echo "Creating Broker with args $PRINT_ARGS"
+		$AMQ_HOME/bin/artemis create broker $AMQ_ARGS
     fi
 
 }
--- modules/amq-install/install.sh
+++ modules/amq-install/install.sh
@@ -3,11 +3,13 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts
+SCRIPT_DIR=$(dirname $0)
+ADDED_DIR=${SCRIPT_DIR}/added
 
 DISTRIBUTION_VERSION="amq-broker-7.1.0"
 
 unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}-bin.zip"
 mv $DISTRIBUTION_VERSION $AMQ_HOME
 
-cp $(dirname $0)/launch.sh $AMQ_HOME/bin
+cp $ADDED_DIR/launch.sh $AMQ_HOME/bin
 chmod 0755 $AMQ_HOME/bin/launch.sh

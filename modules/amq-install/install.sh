bface939a87034e0eb150d7f5bc2289bd2bc5520
Roddie Kieley <rkieley@unifiedsoftworx.com>
2018-09-20 15:06:07 -0230

    ENTMQBR-1996: Updated container image build files to use latest 7.2.1.CR5 AMQ Broker 7.2 distribution.

--- modules/amq-install/install.sh
+++ modules/amq-install/install.sh
@@ -6,9 +6,9 @@ SOURCES_DIR=/tmp/artifacts
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 
-DISTRIBUTION_VERSION="amq-broker-7.2.0"
+DISTRIBUTION_VERSION="amq-broker-7.2.1"
 
-unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}-bin.zip"
+unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}.CR5-bin.zip"
 mv $DISTRIBUTION_VERSION $AMQ_HOME
 
 cp $ADDED_DIR/launch.sh $AMQ_HOME/bin

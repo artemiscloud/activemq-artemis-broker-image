36497e9e0ea8a6831da8c3d0e736f9c7c3c8ec06
Howard Gao <howard.gao@gmail.com>
2020-12-02 17:25:17 +0800

    fix install module referencing old version of artemis

--- modules/activemq-artemis-install/install.sh
+++ modules/activemq-artemis-install/install.sh
@@ -7,7 +7,7 @@ SOURCES_DIR=/tmp/artifacts
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 
-DISTRIBUTION_VERSION="apache-artemis-2.15.0"
+DISTRIBUTION_VERSION="apache-artemis-2.16.0"
 
 unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}-bin.zip"
 mv $DISTRIBUTION_VERSION $AMQ_HOME

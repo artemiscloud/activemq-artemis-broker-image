ee61b519f3b8435dead329da0ce3655cf72683b7
Domenico Francesco Bruscino <brusdev@apache.org>
2020-12-24 15:03:25 +0100

    Expose broker acceptors (#12)

--- modules/activemq-artemis-install/added/launch.sh
+++ modules/activemq-artemis-install/added/launch.sh
@@ -13,13 +13,15 @@ function configure() {
     export CONTAINER_ID=$HOSTNAME
     
     if [ ! -d "BROKER" -o "$AMQ_RESET_CONFIG" = "true" ]; then
-        AMQ_ARGS="--role $AMQ_ROLE --name $AMQ_NAME --allow-anonymous --http-host $BROKER_IP --host $BROKER_IP "
+        AMQ_ARGS="--role $AMQ_ROLE --name $AMQ_NAME --allow-anonymous --http-host $BROKER_IP "
     	if [ -n "${AMQ_USER}" -a -n "${AMQ_PASSWORD}" ] ; then
 			AMQ_ARGS="--user $AMQ_USER --password $AMQ_PASSWORD $AMQ_ARGS "
 		fi
         if [ "$AMQ_CLUSTERED" = "true" ]; then
             echo "Broker will be clustered"
-            AMQ_ARGS="$AMQ_ARGS --clustered --cluster-user $AMQ_CLUSTER_USER --cluster-password $AMQ_CLUSTER_PASSWORD"
+            AMQ_ARGS="$AMQ_ARGS --clustered --cluster-user $AMQ_CLUSTER_USER --cluster-password $AMQ_CLUSTER_PASSWORD --host $BROKER_IP"
+        else
+            AMQ_ARGS="$AMQ_ARGS --host 0.0.0.0"
         fi
         if [ "$AMQ_RESET_CONFIG" ]; then
             AMQ_ARGS="$AMQ_ARGS --force"

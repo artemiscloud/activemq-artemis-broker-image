57157eb7dbbc3beaaa6143c4b5876497ccb51065
Howard Gao <howard_gao@gmail.com>
2021-08-11 21:40:35 +0800

    Update version to 0.1.4 Update broker to 2.18

--- modules/activemq-artemis-install/install.sh
+++ modules/activemq-artemis-install/install.sh
@@ -7,7 +7,7 @@ SOURCES_DIR=/tmp/artifacts
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 
-DISTRIBUTION_VERSION="apache-artemis-2.17.0"
+DISTRIBUTION_VERSION="apache-artemis-2.18.0"
 
 unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}-bin.zip"
 mv $DISTRIBUTION_VERSION $AMQ_HOME

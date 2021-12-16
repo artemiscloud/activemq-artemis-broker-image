bbfa15e5a8a565c04c9eef093ca1f22130233fd3
Domenico Francesco Bruscino <brusdev@apache.org>
2021-12-16 06:53:29 +0100

    Refactor activemq.artemis.install module

--- modules/activemq-artemis-install/install.sh
+++ modules/activemq-artemis-install/install.sh
@@ -7,10 +7,9 @@ SOURCES_DIR=/tmp/artifacts
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 
-DISTRIBUTION_VERSION="apache-artemis-2.19.0"
-
-unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}-bin.zip"
-mv $DISTRIBUTION_VERSION $AMQ_HOME
+unzip -q "$SOURCES_DIR/apache-artemis-bin.zip" -d apache-artemis-bin
+mv "$(ls -d apache-artemis-bin/*)" $AMQ_HOME
+rmdir apache-artemis-bin
 
 cp $ADDED_DIR/launch.sh $AMQ_HOME/bin
 chmod 0755 $AMQ_HOME/bin/launch.sh
